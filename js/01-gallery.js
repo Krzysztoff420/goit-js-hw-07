635cdb3ae31ef0fc7ef44a1ca0715413e298b997
Krzysztof Flisikowski <kflisikowski@10g.pl>
2022-11-20 17:16:29 +0100

    widać galerię

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const item = galleryItems
+  .map(({ preview, original, description }) => 
+    `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", item);
