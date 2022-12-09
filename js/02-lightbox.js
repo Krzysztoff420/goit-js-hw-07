06239227b7ab22a5b9e9b1f7dbd7ae0e9df1a305
Krzysztof Flisikowski <kflisikowski@10g.pl>
2022-12-09 16:14:30 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
@@ -20,4 +20,5 @@ list.insertAdjacentHTML("afterbegin", item);
 
 let lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
+  captionDelay: 250,
 });
